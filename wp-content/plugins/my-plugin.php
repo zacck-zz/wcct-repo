df6d13cb9131caaeae1d025407afa33e2501837d
Zacck <zacck@Zaccks-MBP.riverclub.local>
2016-09-08 14:31:55 +0200

    add custom plugin and build settings page for plugin

--- wp-content/plugins/my-plugin.php
+++ wp-content/plugins/my-plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package Hello_Zacck
- * @version 0.0.1
+ * @version 0.1.1
  */
 /*
-Plugin Name: Zacck's Plugin
+Plugin Name: Zacck's Settings Plugin
 Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Zacck Osiemo
@@ -21,3 +21,28 @@ function myfunction() {
   die();
 }
 */
+
+/*add options page*/
+function zacck_plugin_settings() {
+  add_options_page(
+    'Zacck',
+    'zacck',
+    'manage_options',
+    'zacck_plugin',
+    'zacck_render_settings_page' //callback for page function
+  );
+}
+//Register options page to the menu
+add_filter('admin_menu', 'zacck_plugin_settings');
+
+function zacck_render_settings_page() {
+  ?>
+  <div class="wrap">
+    <h1><php _e('Zacck\'s Settings Page', 'plugin-textdomain')?></h1>
+    <?php settings_error();?>
+    <form method="post" actions="options.php">
+
+    </form>
+  </div>
+  <?php
+}
