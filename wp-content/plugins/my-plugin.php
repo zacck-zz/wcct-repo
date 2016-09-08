5527bacf1a8e33ff21872f154b70f8bd187c2f2f
Zacck <zacck@Zaccks-MBP.riverclub.local>
2016-09-08 11:21:10 +0200

    add coming soon plugin did primary settings

--- wp-content/plugins/my-plugin.php
+++ wp-content/plugins/my-plugin.php
@@ -7,7 +7,17 @@
 Plugin Name: Zacck's Plugin
 Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
+Author: Zacck Osiemo
 Version: 1.6
 Author URI: http://github.com/zacck
+*/
+
+/*hooks & filters
+
+add_action('admin_init', 'myfunction');
+
+function myfunction() {
+  echo 1+1;
+  die();
+}
 */
